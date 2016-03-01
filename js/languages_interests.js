08bba2f80383d16e623985c80db0ab919a6101f4
Stan <stanhuang2017@u.northwestern.edu>
2016-02-29 20:13:37 -0600

    Added to Languages and Interests to allow users to pick options

---
+++ js/languages_interests.js
@@ -0,0 +1,23 @@
+function addRow(tableID, clickedButton) {
+ 
+            var table = document.getElementById(tableID);
+ 
+            var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+ 
+            var cell1 = row.insertCell(0);
+			cell1.className = 'tableCell';
+            
+			cell1.innerHTML = clickedButton;
+			row.insertCell(1).innerHTML= '<button class="btn languageButton" type="button" value = "Delete" onclick="Javascript:deleteRow(this)">';
+ 
+        }
+		
+		function deleteRow(obj) {
+			
+			
+			 var index = obj.parentNode.parentNode.rowIndex;
+			var table = document.getElementById("chosenTable");
+			table.deleteRow(index);
+		   
+		}
